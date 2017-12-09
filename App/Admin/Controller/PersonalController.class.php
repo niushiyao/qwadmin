42f9868230fa1990707295fe7f716f7fe559c1cb
niushiyao <niushiyaohero@163.com>
2017-12-09 13:36:52 +0800

    将其本地化

--- App/Admin/Controller/PersonalController.class.php
+++ App/Admin/Controller/PersonalController.class.php
@@ -1,12 +1,8 @@
 <?php
 /**
- *
- * 版权所有：恰维网络<qwadmin.qiawei.com>
- * 作    者：寒川<hanchuan@qiawei.com>
- * 日    期：2015-09-18
- * 版    本：1.0.0
+ * 作    者：niushiyao
+ * 日    期：2017-12-09
  * 功能说明：个人中心控制器。
- *
  **/
 
 namespace Qwadmin\Controller;
